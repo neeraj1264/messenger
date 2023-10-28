9bb340a4e24086f0e5d67665e60866fe8263f0d0
Neeraj Manchanda <manchandaneeraj396@gmail.com>
2023-10-28 18:42:52 +0530

    avatar set

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Chat from './pages/chat';
 import Login from './pages/login';
+import SetAvatar from "./components/SetAvatar"; 
 export default function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-        {/* <Route path="/setAvatar" element={<SetAvatar />} /> */}
+        <Route path="/setAvatar" element={<SetAvatar />} />
         <Route path="/" element={<Chat />} />
       </Routes>
       </BrowserRouter>
